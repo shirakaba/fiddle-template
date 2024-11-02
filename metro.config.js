3d0dfcebba69e056ffc5a85e4e5af98c911b2fb4
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-11-02 11:40:14 +0900

    integrate @rnx-kit/metro-config

---
+++ metro.config.js
@@ -0,0 +1,5 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+const { makeMetroConfig } = require('@rnx-kit/metro-config');
+
+const config = getDefaultConfig(__dirname);
+module.exports = makeMetroConfig(config);
