23afd1349105217af104bb71dc728b31497fb8a0
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-11-11 13:05:02 +0900

    fix macOS entrypoint (load from Expo's virtual-metro-entry and use their build phase)

--- App.tsx
+++ App.tsx
@@ -7,18 +7,18 @@
 
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import DOMComponent from './src/my-component';
+// import DOMComponent from './src/my-component';
 
 function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <View style={styles.container}>
+      {/* <View style={styles.container}>
         <DOMComponent
           dom={{
             scrollEnabled: false,
           }}
         />
-      </View>
+      </View> */}
       <View style={[styles.container, styles.blue]}>
         <Text style={styles.text}>Native</Text>
       </View>
--- macos/Fiddle-macOS/AppDelegate.mm
+++ macos/Fiddle-macOS/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-  self.moduleName = @"Fiddle";
+  self.moduleName = @"main";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
@@ -22,7 +22,7 @@
 - (NSURL *)bundleURL
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
