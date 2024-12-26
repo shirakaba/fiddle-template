e055abd24a2dbcd95a91dbaee002c134d59da2d9
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-12-26 16:33:03 +0000

    get Expo's registerRootComponent() working by patching expo-asset

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @format
- */
+import { registerRootComponent } from 'expo';
 
 import App from './App';
-import {AppRegistry} from 'react-native';
 
-AppRegistry.registerComponent('main', () => App);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
