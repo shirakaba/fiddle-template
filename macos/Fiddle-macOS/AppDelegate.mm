078b5e84903a03e1cf179fa0ae55b71fc8c672fc
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-12-26 18:10:38 +0000

    give up on ExpoAppDelegate (easier to patch ExpoModulesCore)
    See patches/expo-modules-core+2.0.0-preview.10.patch from the earlier commit "update npm deps".

--- macos/Fiddle-macOS/AppDelegate.mm
+++ macos/Fiddle-macOS/AppDelegate.mm
@@ -2,25 +2,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 
-#if __has_include(<Fiddle/Fiddle-Swift.h>)
-// When use_frameworks! is enabled
-#import <Fiddle/Fiddle-Swift.h>
-#else
-// When use_frameworks! is disabled
-#import "Fiddle-Swift.h"
-#endif
-
-@implementation AppDelegate {
-  EXExpoAppDelegateMacOS *_expoAppDelegate;
-}
-
-- (instancetype)init
-{
-  if (self = [super init]) {
-    _expoAppDelegate = [[EXExpoAppDelegateMacOS alloc] init];
-  }
-  return self;
-}
+@implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
@@ -29,12 +11,7 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
   
-  // Here, we inline the contents of EXAppDelegateWrapper's method `- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions`.
-  [super applicationDidFinishLaunching:notification];
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-result"
-  [_expoAppDelegate applicationDidFinishLaunching:notification];
-#pragma clang diagnostic pop
+  return [super applicationDidFinishLaunching:notification];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
